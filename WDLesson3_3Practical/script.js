1a8223cc347fcf6e877b9829f58485531cbee89c
xavierf86 <xavierf86@nycstudents.net>
2025-12-23 20:17:23 +0000

    update practical 3.3

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,16 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function max(){
+    document.getElementById("highlight").innerHTML="This is Max Verstappen";
+}
+function lando(){
+    document.getElementById("highlight").innerHTML="This is Lando Norris"
+}
+function leclerc(){
+    document.getElementById("highlight").innerHTML="This is Charles Leclerc";
+}
+function piastri(){
+    document.getElementById("highlight").innerHTML="This is Oscar Piastri"
+}
+function bellingham(){
+    document.getElementById("highlight").innerHTML="This is Jude Bellingham";
+}
