c8e64893036fe6c1be3029e217e46e395b750b76
xavierf86 <xavierf86@nycstudents.net>
2026-01-22 14:33:07 +0000

    update practical 3.7

--- WDLesson3_7Practical/script.js
+++ WDLesson3_7Practical/script.js
@@ -3,7 +3,18 @@
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
+function distance(){
+      let x1=parseFloat(document.getElementById("x1").value);
+      let x2=parseFloat(document.getElementById("x2").value);
+      let y1=parseFloat(document.getElementById("y1").value);
+      let y2=parseFloat(document.getElementById("y2").value);
+      let output=document.getElementById("output");
+      let v1=(x2 - x1);
+      let v2=(y2 - y1);
+      let d=Math.sqrt(Math.pow(v1,2) + Math.pow(v2,2));
 
+      output.innerHTML= d;
+}
 /* Challenge 4: Create a function to serve as the event handler for the compound interest UI. Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
       2) Perform the necessary calculations
