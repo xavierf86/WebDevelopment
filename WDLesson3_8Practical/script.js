bea5b588cc69bd8896e4b2a2c9b604ca70b36ea4
xavierf86 <xavierf86@nycstudents.net>
2026-01-29 14:33:23 +0000

    update practical 3.8

--- WDLesson3_8Practical/script.js
+++ WDLesson3_8Practical/script.js
@@ -3,19 +3,23 @@
 
 
 /* Challenge 4: Does the function name match the event handler referenced in the event listener? */
-function volume(){
-
+function cylinder(){
+  /*The function was wrong, it should be cylinder()*/
   /* Challenge 5: Are there any errors in retrieving and parsing the information from the text inputs? */
-  let r = parseInt(document.getElementBy("r").value);
-  let height = parseInt(document.getElementbyId("height").value);
+  let r = parseInt(document.getElementById("r").value);
+  let h = parseInt(document.getElementById("h").value);
+
 
   /* Challenge 6: Does the variable output correctly create a reference to the output container? */
-  let output = document.getElementById("output").value;
+  let output = document.getElementById("output");
+  /*this has .value, when it shouldnt*/
 
   /* Challenge 7: Is the following calculation for volume of a cylinder correct? */
-  let v = math.PI * math.pow(r,2) / h;
+  let v = Math.PI * Math.pow(r,2) * h;
+  /*This should be multiplying by h, not divinding, and math was lowercased, should be Math*/
 
   /* Challenge 8: Are there any errors in displaying the output? */
-  output.innerHTML = `Volume of the cylinder is ${v} 
+  output.innerHTML = "Volume of the cylinder is " + v
+  /* Fix the quotation marks, and taek the v out of the brackets, with it being outside the quotation marks.*/
 
 }
