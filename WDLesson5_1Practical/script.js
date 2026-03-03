f06b8445a70893c2e717639fbbddf7355ac44486
xavierf86 <xavierf86@nycstudents.net>
2026-03-03 14:01:59 +0000

    update unit 5

---
+++ WDLesson5_1Practical/script.js
@@ -0,0 +1,14 @@
+/* Challenge 2: Create the function that will calculate the balance in an account using compound 
+interest after t number of years.
+
+Notes:
+1) Don't let the formula scare you.  Simply translate it into JS
+2) Create a for loop where the loop variable is t and the condition ends at the number of years 
+the user specifies in the text input.
+3) Use a build variable to create an output that looks like this
+Year 1: $xxxxxx
+Year 2: $xxxxxx
+Year 3: $xxxxxx
+...
+Year N: $xxxxxx
+*/
