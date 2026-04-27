32af67d501f2b4124389dac09a22099ba8013701
xavierf86 <xavierf86@nycstudents.net>
2026-04-27 13:33:55 +0000

    update 6.7

---
+++ WDLesson6_7Practical/script.js
@@ -0,0 +1,81 @@
+let data;
+async function init(){
+  
+  //let link = "https://data.cityofnewyork.us/resource/h9gi-nx95.json"
+  let link = "311.json"
+  info = await fetch(link);
+  data = await info.json();
+  
+  let output = document.getElementById("output");
+  let build = "";
+
+  for(let i = 0; i < data.length; i+=1){
+    let complaint = data[i];
+    build += `<div class="fitted card">
+                 <h3></h3>
+                 <hr>
+                 <p></p>
+                 <p></p>
+                 <p></p>
+                 <hr>
+                 <p></p>
+                 <hr>
+                 <p></p>
+              </div>`    
+  }
+  output.innerHTML = build;
+}
+
+function filterByBorough(){
+  let output = document.getElementById("output");
+  let borough = document.getElementById("borough").value;
+  let result = document.getElementById("result");
+  let build = "";
+  let ct = 0;
+
+  for(let i = 0; i < data.length; i+=1){
+    let complaint = data[i];
+    if(complaint.borough == borough){
+      build += `<div class="fitted card">
+                    <h3>${complaint.complaint_type}</h3>
+                    <hr>
+                    <p>${complaint.borough}</p>
+                    <p>${complaint.incident_zip}</p>
+                    <p>${complaint.descriptor}</p>
+                    <hr>
+                    <p>${complaint.created_date}</p>
+                    <hr>
+                    <p>${complaint.agency}</p>
+                </div>`;
+      ct += 1;
+    }
+  }
+  result.innerHTML = `${ct} Results found.`
+  output.innerHTML = build;
+}
+
+function filterByZip(){
+  let output = document.getElementById("output");
+  let zip = document.getElementById("zip");
+  let result = document.getElementById("result");
+  
+  let build = "";
+  for(let i = 0; i < data.length; i +=1){
+    let complaint = data[i]
+    if(complaint.incident_zip == zip.value){
+      build = `<div class = "fitted card">
+                      <h3>${complaint.complaint_type}</h3>
+                      <hr>
+                      <p>${complaint.borough}</p>
+                      <p>${complaint.incident_zip}</p>
+                      <p>${complaint.descriptor}</p>
+                      <hr>
+                      <p>${complaint.created_date}</p>
+                      <hr
+                      <p>${complaint.agency}</p>
+                </div>`
+                
+    }
+  }
+  output.innerHTML = build;
+}
