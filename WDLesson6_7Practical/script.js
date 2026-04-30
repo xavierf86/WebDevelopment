a8e415058447070d1cb0d7254a022619fc41c417
xavierf86 <xavierf86@nycstudents.net>
2026-04-30 13:34:04 +0000

    update practical 6.7

--- WDLesson6_7Practical/script.js
+++ WDLesson6_7Practical/script.js
@@ -2,7 +2,7 @@ let data;
 async function init(){
   
   //let link = "https://data.cityofnewyork.us/resource/h9gi-nx95.json"
-  let link = "311.json"
+  let link = "collision.json"
   info = await fetch(link);
   data = await info.json();
   
@@ -12,41 +12,81 @@ async function init(){
   for(let i = 0; i < data.length; i+=1){
     let collision = data[i];
     build += `<div class="fitted card">
-                 <h3>${collision.contributing_factor_vehicle_1}</h3>
+                 <h3>${collision.crash_date}</h3>
                  <hr>
-                 <p>${collision.borough}</p>
-                 <p>${collision.zip_code}</p>
-                 <p>${collision.on_street_name}</p>
+                 <p>Crash Time: ${collision.crash_time}</p>
+                 <p>Cause of Crash: ${collision.contributing_factor_vehicle_1}</p>
+                 <p>Zip Code: ${collision.zip_code}</p>
+                 <p>Street: ${collision.on_street_name}</p>
+                 <P>Off Street: ${collision.off_street_name} </p>
                  <hr>
-                 <p>${collision.crash_date}</p>
+                 <p>People Injured: ${collision.number_of_persons_injured}</p>
+                 <p>People Killed: ${collision.number_of_persons_killed}</p>
                  <hr>
-                 <p></p>
+                 <p>Vehicle Type: ${collision.vehicle_type_code1}</p>
+                 <p>Collision ID: ${collision.collision_id} </p>
               </div>`    
   }
   output.innerHTML = build;
 }
 
-function filterByBorough(){
+function filterByTime(){
   let output = document.getElementById("output");
-  let borough = document.getElementById("borough").value;
+  let time = document.getElementById("time").value;
   let result = document.getElementById("result");
   let build = "";
   let ct = 0;
 
   for(let i = 0; i < data.length; i+=1){
-    let complaint = data[i];
-    if(complaint.borough == borough){
+    let collision = data[i];
+    if(collision.crash_time == time){
       build += `<div class="fitted card">
-                    <h3>${complaint.complaint_type}</h3>
+                    <h3>${collision.crash_date}</h3>
                     <hr>
-                    <p>${complaint.borough}</p>
-                    <p>${complaint.incident_zip}</p>
-                    <p>${complaint.descriptor}</p>
+                    <p>Crash Time: ${collision.crash_time}</p>
+                    <p>Cause of Crash: ${collision.contributing_factor_vehicle_1}</p>
+                    <p>Zip Code: ${collision.zip_code}</p>
+                    <p>Street: ${collision.on_street_name}</p>
+                    <p>Off Street: ${collision.off_street_name} </p>
                     <hr>
-                    <p>${complaint.created_date}</p>
+                    <p>People Injured: ${collision.number_of_persons_injured}</p>
+                    <p>People Killed: ${collision.number_of_persons_killed}</p>
                     <hr>
-                    <p>${complaint.agency}</p>
-                </div>`;
+                    <p>Vehicle Type: ${collision.vehicle_type_code1}</p>
+                    <p>Collision ID: ${collision.collision_id} </p>
+                </div>` 
+      ct += 1;
+    }
+  }
+  result.innerHTML = `${ct} Results found.`
+  output.innerHTML = build;
+}
+
+function filterByInjured(){
+  let output = document.getElementById("output");
+  let injured = document.getElementById("injured").value;
+  let result = document.getElementById("result");
+  let build = "";
+  let ct = 0;
+
+  for(let i = 0; i < data.length; i+=1){
+    let collision = data[i];
+    if(collision.crash_time == time){
+      build += `<div class="fitted card">
+                    <h3>${collision.crash_date}</h3>
+                    <hr>
+                    <p>Crash Time: ${collision.crash_time}</p>
+                    <p>Cause of Crash: ${collision.contributing_factor_vehicle_1}</p>
+                    <p>Zip Code: ${collision.zip_code}</p>
+                    <p>Street: ${collision.on_street_name}</p>
+                    <p>Off Street: ${collision.off_street_name} </p>
+                    <hr>
+                    <p>People Injured: ${collision.number_of_persons_injured}</p>
+                    <p>People Killed: ${collision.number_of_persons_killed}</p>
+                    <hr>
+                    <p>Vehicle Type: ${collision.vehicle_type_code1}</p>
+                    <p>Collision ID: ${collision.collision_id} </p>
+                </div>` 
       ct += 1;
     }
   }
@@ -63,19 +103,24 @@ function filterByZip(){
   for(let i = 0; i < data.length; i +=1){
     let complaint = data[i]
     if(complaint.incident_zip == zip.value){
-      build = `<div class = "fitted card">
-                      <h3>${complaint.complaint_type}</h3>
-                      <hr>
-                      <p>${complaint.borough}</p>
-                      <p>${complaint.incident_zip}</p>
-                      <p>${complaint.descriptor}</p>
-                      <hr>
-                      <p>${complaint.created_date}</p>
-                      <hr
-                      <p>${complaint.agency}</p>
-                </div>`
-                
+      build += `<div class="fitted card">
+                    <h3>${collision.crash_date}</h3>
+                    <hr>
+                    <p>Crash Time: ${collision.crash_time}</p>
+                    <p>Cause of Crash: ${collision.contributing_factor_vehicle_1}</p>
+                    <p>Zip Code: ${collision.zip_code}</p>
+                    <p>Street: ${collision.on_street_name}</p>
+                    <p>Off Street: ${collision.off_street_name} </p>
+                    <hr>
+                    <p>People Injured: ${collision.number_of_persons_injured}</p>
+                    <p>People Killed: ${collision.number_of_persons_killed}</p>
+                    <hr>
+                    <p>Vehicle Type: ${collision.vehicle_type_code1}</p>
+                    <p>Collision ID: ${collision.collision_id} </p>
+                </div>` 
+        ct+=1
     }
   }
+  result.innerHTML = `${ct} Results found.`
   output.innerHTML = build;
 }
